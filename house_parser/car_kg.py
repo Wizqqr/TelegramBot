99ef0ddb8bd6e9166940464d7baadc5b0ea3772d
Wizqqr <baban1979molotov@gmail.com>
2024-04-28 23:10:26 +0600

    dz6

--- house_parser/car_kg.py
+++ house_parser/car_kg.py
@@ -14,28 +14,44 @@ class HouseCrawler:
     async def get_page(self):
         async with httpx.AsyncClient() as client:
             response = await client.get(self.MAIN_URL)
-            self.page_content = response.text
-            return response
+            if response.status_code == 200:
+                self.page_content = response.text
+                return response
+            else:
+                return None
 
     async def get_title(self):
-        await self.get_page()
-        html = Selector(self.page_content)
-        title = html.css("title::text").get()
-        return title
+        page_response = await self.get_page()
+        if page_response:
+            html = Selector(self.page_content)
+            title = html.css("title::text").get()
+            return title
+        else:
+            return None
 
     async def get_property_links(self):
-        await self.get_page()
-        html = Selector(self.page_content)
-        links = html.css('.property-list .property-item a::attr(href)').getall()
-        full_links = [self.BASE_URL + link for link in links]
-        return full_links
+        page_response = await self.get_page()
+        if page_response:
+            html = Selector(self.page_content)
+            if html.css('.property-list .property-item a::attr(href)').get():
+                links = html.css('.property-list .property-item a::attr(href)').getall()
+                full_links = [self.BASE_URL + link for link in links]
+                return full_links
+            else:
+                return None
+        else:
+            return None
 
 
-    @car_router.message(Command("links"))
-    async def links(message: types.Message):
-        crawler = HouseCrawler()
-        await crawler.get_page()
-        title = await crawler.get_title()
+@car_router.message(Command("links"))
+async def links(message: types.Message):
+    crawler = HouseCrawler()
+    title = await crawler.get_title()
+    if title:
         link = await crawler.get_property_links()
-        await message.answer(f"Имя: {title}, ссылка {', '.join(link)}")
-
+        if link:
+            await message.answer(f"Имя: {title}, ссылка {link}")
+        else:
+            await message.answer("Не удалось найти ссылки на объекты")
+    else:
+        await message.answer("Не удалось получить заголовок страницы")
